b00ad9290cb051de411a7ede79f82f56015fcee3
George Jauga <georgejauga37@gmail.com>
2022-06-06 01:11:15 +0300

    index.html ,script.js,style.css added.

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const h2 = document.createElement('h2')
 h2.textContent = 'By Jakababa'
 
 document.querySelector('body').appendChild(h2)
+const h4 = document.createElement('h4')
+h2.textContent = 'Rich in Value ,Cheap in Price'
